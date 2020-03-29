537c3295fb572d5086a14ea17c178c20214b5984
Ehsan1998 <e.ares77@gmail.com>
2020-03-30 01:04:13 +0430

    VerificationCodeActivity

---
+++ app/src/main/java/com/postpay/Activities/VerificationCodeActivity.java
@@ -0,0 +1,16 @@
+package com.postpay.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.postpay.R;
+
+public class VerificationCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification_code);
+    }
+}
