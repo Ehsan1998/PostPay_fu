dfbcf2986b83a4c34ce4c607d7fb182a25c34ff8
Ehsan1998 <e.ares77@gmail.com>
2020-04-08 05:21:30 +0430

    send code

--- app/src/main/java/com/postpay/Activities/GetPhoneNumberActivity.java
+++ app/src/main/java/com/postpay/Activities/GetPhoneNumberActivity.java
@@ -2,15 +2,72 @@ package com.postpay.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 
 import com.postpay.R;
 
+import br.com.sapereaude.maskedEditText.MaskedEditText;
+
 public class GetPhoneNumberActivity extends AppCompatActivity {
+
+    //MaskedEditText maskedEditText;
+    br.com.sapereaude.maskedEditText.MaskedEditText maskedEditText;
+    Button button;
+    private String phoneNumber;
+    private String code = "09";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_phone_number);
+
+        maskedEditText = (MaskedEditText) findViewById(R.id.phoneInput2);
+        button = (Button) findViewById(R.id.phoneButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnClicked();
+            }
+        });
+
+        maskedEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                phoneNumber = code + maskedEditText.getRawText();
+                if (phoneNumber.length() == 11){
+                    button.setBackgroundResource(R.drawable.enable_button);
+                    button.setEnabled(true);
+                    button.setTextColor(getResources().getColor(R.color.colorAccent));
+                }
+                else{
+                    button.setBackgroundResource(R.drawable.disable_button);
+                    button.setEnabled(false);
+                    button.setTextColor(getResources().getColor(R.color.gray500));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+    }
+
+    private void btnClicked() {
+        Intent intent = new Intent(this , VerificationCodeActivity.class);
+        intent.putExtra("phoneNumber",phoneNumber);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/postpay/Activities/VerificationCodeActivity.java
+++ app/src/main/java/com/postpay/Activities/VerificationCodeActivity.java
@@ -3,6 +3,7 @@ package com.postpay.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.postpay.R;
 
@@ -12,5 +13,9 @@ public class VerificationCodeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification_code);
+
+        TextView textView = findViewById(R.id.txt);
+        Bundle bundle = getIntent().getExtras();
+        textView.setText(bundle.getString("phoneNumber"));
     }
 }
